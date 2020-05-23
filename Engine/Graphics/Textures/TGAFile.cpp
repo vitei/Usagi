871a72eddb9cd01aeda996275cd8de882368e3a4
Alex Miyamoto <alexgamesdev@gmail.com>
2020-05-23 13:42:57 +0900

    Add support for grayscale debug images

--- Engine/Graphics/Textures/TGAFile.cpp
+++ Engine/Graphics/Textures/TGAFile.cpp
@@ -44,9 +44,13 @@ namespace usg
 		m_header.uWidth = uWidth;
 		m_header.uHeight = uHeight;
 		uint32 uSrcOffset = eFormat == CF_RGBA_8888 ? 4 : 3;
-		m_header.uBitsPerPixel = 24;	
-		ASSERT(eFormat == CF_RGBA_8888 || eFormat == CF_RGB_888);
-		m_uFileSize = uWidth * uHeight * 3;
+		if (eFormat == CF_R_8)
+		{
+			uSrcOffset = 1;
+		}
+		m_header.uBitsPerPixel = 24;
+		ASSERT(eFormat == CF_RGBA_8888 || eFormat == CF_RGB_888 || eFormat == CF_R_8);
+		m_uFileSize = (memsize)(uWidth * uHeight * 3);
 
 		ASSERT(m_pData == NULL);
 		m_pData = (uint8*)mem::Alloc(MEMTYPE_STANDARD, ALLOC_DEBUG, m_uFileSize);
@@ -57,9 +61,18 @@ namespace usg
 		uint32 j = 0;
 		while (i < m_uFileSize)
 		{
-			m_pData[i+0] = pSrc[j+2];       //grab blue
-			m_pData[i+1] = pSrc[j+1];		//assign red to blue
-			m_pData[i+2] = pSrc[j+0];		//assign blue to red
+			if(eFormat != CF_R_8)
+			{
+				m_pData[i+0] = pSrc[j+2];       //grab blue
+				m_pData[i+1] = pSrc[j+1];		//assign red to blue
+				m_pData[i+2] = pSrc[j+0];		//assign blue to red
+			}
+			else
+			{
+				m_pData[i + 0] = pSrc[j];
+				m_pData[i + 1] = pSrc[j];
+				m_pData[i + 2] = pSrc[j];
+			}
 
 			i += 3;     //skip to next blue byte
 			j += uSrcOffset;
