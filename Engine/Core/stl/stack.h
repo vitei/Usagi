3fd33631a19eebbb5efc837ce0aea8306c9d4e35
Alex Miyamoto <alexgamesdev@gmail.com>
2021-01-11 12:26:50 +0900

    Fix the binding for the eastl stack

--- Engine/Core/stl/stack.h
+++ Engine/Core/stl/stack.h
@@ -3,12 +3,13 @@
 #define USAGI_STL_STACK_H
 
 #include "Engine/Core/stl/usagi_eastl_allocator.h"
+#include "vector.h"
 #include <EASTL/stack.h>
 
 namespace usg
 {
-	template<typename T, typename Allocator = USAGISTLAllocatorType>
-	using stack = eastl::stack<T, Allocator>;
+	template<typename T, typename Container = usg::vector<T>>
+	using stack = eastl::stack<T, Container>;
 };
 
-#endif // USAGI_STL_VECTOR_H
+#endif // USAGI_STL_STACK_H
