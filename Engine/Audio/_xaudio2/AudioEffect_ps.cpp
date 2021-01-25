d30f63689464764c429bb719d15f729210533c1d
Alex Miyamoto <alexgamesdev@gmail.com>
2021-01-25 17:22:03 +0900

    Switch over the reverb struct to the default one

--- Engine/Audio/_xaudio2/AudioEffect_ps.cpp
+++ Engine/Audio/_xaudio2/AudioEffect_ps.cpp
@@ -31,23 +31,30 @@ namespace usg{
 		}
 		
 		ReverbEffectDef* pReverb = (ReverbEffectDef*)pDef;
-		XAUDIO2FX_REVERB_I3DL2_PARAMETERS paramsIn;
 
-		paramsIn.DecayHFRatio = pReverb->decayHFRatio;
-		paramsIn.DecayTime = pReverb->decayTime;
-		paramsIn.Density = pReverb->density;
-		paramsIn.Diffusion = pReverb->diffusion;
-		paramsIn.HFReference = pReverb->hfReference;
-		paramsIn.Reflections = pReverb->reflections;
-		paramsIn.ReflectionsDelay = pReverb->reflectionsDelay;
-		paramsIn.Reverb = pReverb->reverb;
-		paramsIn.ReverbDelay = pReverb->reverbDelay;
-		paramsIn.Room = pReverb->room;
-		paramsIn.RoomHF = pReverb->roomHF;
-		paramsIn.RoomRolloffFactor = pReverb->roomRolloffFactor;
-		paramsIn.WetDryMix = pReverb->wetDryMix;
-
-		ReverbConvertI3DL2ToNative(&paramsIn, &m_params);
+		memset(&m_params, 0, sizeof(m_params));
+		m_params.DecayTime = pReverb->decayTime;
+		m_params.Density = pReverb->density;
+		m_params.DisableLateField = XAUDIO2FX_REVERB_DEFAULT_DISABLE_LATE_FIELD;
+		m_params.EarlyDiffusion = XAUDIO2FX_REVERB_DEFAULT_EARLY_DIFFUSION;
+		m_params.LateDiffusion = XAUDIO2FX_REVERB_DEFAULT_LATE_DIFFUSION;
+		m_params.HighEQCutoff = XAUDIO2FX_REVERB_DEFAULT_HIGH_EQ_CUTOFF;
+		m_params.LowEQCutoff = XAUDIO2FX_REVERB_DEFAULT_LOW_EQ_CUTOFF;
+		m_params.PositionLeft = XAUDIO2FX_REVERB_DEFAULT_POSITION;
+		m_params.PositionRight = XAUDIO2FX_REVERB_DEFAULT_POSITION;
+		m_params.PositionMatrixLeft = XAUDIO2FX_REVERB_DEFAULT_POSITION_MATRIX;
+		m_params.PositionMatrixRight = XAUDIO2FX_REVERB_DEFAULT_POSITION_MATRIX;
+		m_params.RearDelay = XAUDIO2FX_REVERB_DEFAULT_REAR_DELAY;
+		m_params.ReflectionsDelay = pReverb->reflectionsDelay;
+		m_params.ReflectionsGain = pReverb->reflectionsGain;
+		m_params.ReverbDelay = (BYTE)pReverb->reverbDelay;
+		m_params.ReverbGain = pReverb->reverbGain;
+		m_params.RoomFilterFreq = pReverb->roomFilterFreq;
+		m_params.RoomFilterHF = pReverb->roomFilterHF;
+		m_params.RoomFilterMain = pReverb->roomFilterMain;
+		m_params.RoomSize = pReverb->roomSize;
+		m_params.SideDelay = XAUDIO2FX_REVERB_DEFAULT_7POINT1_SIDE_DELAY;
+		m_params.WetDryMix = pReverb->wetDryMix;
 	}
 
 }
