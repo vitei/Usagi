3596f7eced8390132ebab9f6298cb67c21812ce5
Alex Miyamoto <alexgamesdev@gmail.com>
2021-07-06 11:15:11 +0900

    If a pass reads a buffer it needs to flip it's own target, not the next target

--- Engine/PostFX/_fragment/PostFXSys_ps.cpp
+++ Engine/PostFX/_fragment/PostFXSys_ps.cpp
@@ -593,17 +593,19 @@ void PostFXSys_ps::GetRenderTargetBuffers(memsize pass, usg::vector<ColorBuffer*
 		// FIXME: OR HDR enabled at a system level
 		if(pass < iFinalHdr)
 		{	
-			pBuffers.push_back(&m_colorBuffer[BUFFER_HDR_0 + hdrIdx]);
 			// If it reads that texture as a source we need to swap the buffers
-			if(m_activeEffects[pass]->ReadsTexture(PostEffect::Input::Color))
-				hdrIdx = (hdrIdx + 1)%2;
+			if (m_activeEffects[pass]->ReadsTexture(PostEffect::Input::Color))
+				hdrIdx = (hdrIdx + 1) % 2;
+
+			pBuffers.push_back(&m_colorBuffer[BUFFER_HDR_0 + hdrIdx]);
 		}
 		else
 		{
-			pBuffers.push_back(&m_colorBuffer[BUFFER_LDR_0 + ldrIdx]);
 			// If it reads that texture as a source we need to swap the buffers
 			if (m_activeEffects[pass]->ReadsTexture(PostEffect::Input::Color))
-				ldrIdx = (ldrIdx + 1)%2;
+				ldrIdx = (ldrIdx + 1) % 2;
+
+			pBuffers.push_back(&m_colorBuffer[BUFFER_LDR_0 + ldrIdx]);
 		}
 		ASSERT(!m_activeEffects[pass]->WritesTexture(PostEffect::Input::Albedo));
 	}
