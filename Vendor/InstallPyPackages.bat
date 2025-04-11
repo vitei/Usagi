1421c574bd2f34871ed5ef7ac216cf9353d7c73a
Alex Miyamoto <alexgamesdev@gmail.com>
2025-04-11 21:08:10 +0900

    Temporary workaround to get old python packages

--- Vendor/InstallPyPackages.bat
+++ Vendor/InstallPyPackages.bat
@@ -1,1 +1,19 @@
-python -m pip install -r ..\Tools\python\pypackages.txt
+@echo off
+setlocal enabledelayedexpansion
+
+REM Path to your requirements file
+set REQUIREMENTS=..\Tools\python\pypackages.txt
+
+REM Loop through each line in the requirements file
+for /f "usebackq delims=" %%i in ("%REQUIREMENTS%") do (
+    set "line=%%i"
+    
+    REM Skip empty lines or comment lines
+    if not "!line!"=="" if "!line:~0,1!" neq "#" (
+        echo Installing !line!...
+        easy_install !line!
+    )
+)
+
+echo Done!
+pause
